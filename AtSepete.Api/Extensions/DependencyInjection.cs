ad6223f7812ad09544569c0b2c234a3d9ddcaae4
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-03-23 14:56:08 +0300

    lifetimevalidator eklendi

--- AtSepete.Api/Extensions/DependencyInjection.cs
+++ AtSepete.Api/Extensions/DependencyInjection.cs
@@ -26,7 +26,7 @@ namespace AtSepete.Api.Extensions
                         ValidAudience = configuration["Token:Audience"],
                         ValidIssuer = configuration["Token:Issuer"],
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Token:SecurityKey"])),
-                        LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires!=null?expires>DateTime.UtcNow:false
+                        LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires!=null? expires>DateTime.UtcNow:false
                         //expires=> gelen jwt nin ömrüne bakar.eğer ki süresini doldurmuşsa kullanılamaz.
                     };
                     
