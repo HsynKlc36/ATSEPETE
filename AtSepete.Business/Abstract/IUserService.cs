34dec511b07caa4f2580380b6020b1369b03c143
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-04-03 01:57:58 +0300

    UserService yazılmaya başlandı

--- AtSepete.Business/Abstract/IUserService.cs
+++ AtSepete.Business/Abstract/IUserService.cs
@@ -1,10 +1,14 @@
 ﻿using AtSepete.Dtos.Dto;
 using AtSepete.Entities.Data;
 using AtSepete.Results;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +16,27 @@ namespace AtSepete.Business.Abstract
 {
     public interface IUserService 
     {
-       
+        Task SignInAsync(UserDto user, bool isPersistent, string authenticationMethod = null);
+         Task SignInAsync(UserDto user, AuthenticationProperties authenticationProperties, string authenticationMethod = null);//İkinci parametre olan authenticationProperties özelliği, kullanıcının kimlik doğrulaması işleminin niteliklerini içeren bir nesnedir. Bu nesne, kullanıcının kimlik doğrulama işlemleri sırasında taşıyabileceği ek bilgileri içerir. Örneğin, bu nesne kullanıcının dil ayarlarını, kimlik doğrulama işlemi sırasında kullanabileceği bir token'ı veya diğer kullanıcı bilgilerini içerebilir.Son parametre olan authenticationMethod özelliği, kullanıcının kimlik doğrulama yöntemini belirler.Varsayılan olarak null'dır. Ancak, özel kimlik doğrulama yöntemleri kullanmak isterseniz, bu parametre kullanılabilir.
+        Task PasswordSignInAsync(UserDto user, string password, bool isPersistent, bool lockoutOnFailure);//kullanıcı doğrulama,3.parametre sürekli oturumu açık bırakır,4. parametre ise hatalı girişlerde kullanıcıyı kitler
+        Task PasswordSignInAsync(string userName, string password, bool isPersistent, bool lockoutOnFailure);// kullanıcı doğrulama,3.parametre sürekli oturumu açık bırakır,4. parametre ise hatalı girişlerde kullanıcıyı kitler
+        Task SignOutAsync();
+        //yukarıdaki satırlar giriş ve çıkış işlemleri için kullanılır
+        Task<IDataResult<List<UserDto>>> GetAllUserAsync();//tüm user'ları getirir
+        Task<IDataResult<UserDto>> FindUserByIdAsync(Guid id);// id ye göre user getirir
+        Task<UserDto> GetUserAsync(ClaimsPrincipal principal);// login olan kullanıcıyı getirir
+        Task<IDataResult<UserDto>> FindUserByEmailAsync(string email);//maile göre user getirir
+        Task<IDataResult<UserDto>> AddToRoleAsync(UserDto user, string role);//user ekler
+        Task<IDataResult<List<UserDto>>> FindUsersByRoleAsync(string roleName);//role gore user'ları getirir
+        Task<IDataResult<UserDto>> AddUserAsync(UserDto entity);//user ekleme
+        Task<IDataResult<UserDto>> UpdateUserAsync(Guid id, UserDto userDto);//user güncelleme
+        Task<IResult> ChangePasswordAsync(UserDto user, string currentPassword, string newPassword);//user parolasını değiştirmek
+        Task<IResult> ResetPasswordAsync(UserDto user, string token, string newPassword);//user parolasını sıfırlamak
+        Task<IResult> HardDeleteUserAsync(Guid id);//veritabanından siler
+        Task<IResult> SoftDeleteUserAsync(Guid id);//IsActive false' a çeker
+       Task<IResult> CheckPasswordAsync(UserDto user, string password);//user'ın şifresini kontrol eder!
+
+        //public virtual Task<IdentityResult> AddClaimAsync(TUser user, Claim claim);
+
     }
 }
--- AtSepete.Business/Concrete/UserService.cs
+++ AtSepete.Business/Concrete/UserService.cs
@@ -3,9 +3,13 @@ using AtSepete.Dtos.Dto;
 using AtSepete.Entities.Data;
 using AtSepete.Repositories.Abstract;
 using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,14 +19,167 @@ namespace AtSepete.Business.Concrete
     {
         private readonly IUserRepository _userRepository;
 
-
         public UserService(IUserRepository userRepository,IMapper mapper)
         {
             _userRepository = userRepository;
-
-
         }
 
-        
+
+        public IActionResult CreateUser()
+        {
+            CreatePerson createManager = new CreatePerson();
+            createManager.Companies = _company.GetAll();
+            createManager.Person = new Person();
+            return View(createManager);
+        }
+
+        [HttpPost]
+        public IActionResult CreateManager(CreatePerson createPerson)
+        {
+            var manager = createPerson.Person;
+            var tryMail = _context.Employees.Where(x => x.Mail == createPerson.Person.Mail).Select(y => y.Mail).FirstOrDefault();
+            if (tryMail != null)
+            {
+                ViewBag.AllReadyAddedd = "Mail adresi Sistemde Kayıtlı";
+                return View();
+            }
+            else
+            {
+                if (manager.Photo == null)
+                {
+                    if (manager.Gender == ENTITIES.Enums.GenderEnum.Erkek)
+                    {
+                        manager.PhotoName = "\\img\\3586f868_94a2_4fd2_8933_17cd1ff7605e.jpeg";
+                    }
+                    else
+                    {
+                        manager.PhotoName = "\\img\\3a172567_ce6c_4e78_964a_6860b74eeff1.jpeg";
+                    }
+                }
+                else
+                {
+                    manager.PhotoName = SaveThePicture(manager.Photo);
+                }
+                manager.RoleEnum = RoleEnum.Yönetici;
+                var fromAddress = new MailAddress("i_am_hr@outlook.com");
+                var toAddress = new MailAddress(manager.Mail);
+                var Link = "Şifrenizi Oluşturmak İçin Linke Tıklayınız<a href= http://imhere.azurewebsites.net/Home/ResetPass/" + manager.Mail + ">Buraya Tıklayınız</a>.";
+
+                string resetPass = "Şifre Oluşturma Bağlantınız";
+                using (var smtp = new SmtpClient
+                {
+                    Host = "smtp-mail.outlook.com",
+                    /**/
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+
+                    Credentials = new NetworkCredential(fromAddress.Address, "ik-123456")
+                })
+                    try
+                    {
+                        using (var message = new MailMessage(fromAddress, toAddress) { Subject = resetPass, Body = Link, IsBodyHtml = true })
+                        {
+                            smtp.Send(message);
+                        }
+                        ViewBag.Success = "Mail Başarıyla Gönderildi.";
+                    }
+                    catch (Exception)
+                    {
+                        ViewBag.Unsuccess = "Mail Gönderiminde Hata Oluştu.";
+                        return View();
+                    }
+                _person.Add(manager);
+                return RedirectToAction("GetManager");
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult CreatePackage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePackage(Package package)
+        {
+            if (package.StartDate < package.EndDate)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (package.Photo != null)
+                    {
+                        package.PhotoName = SaveThePicture(package.Photo);
+                    }
+                    else
+                    {
+                        ViewBag.PackageHata = "Fotoğraf Seçiniz";
+                        return View();
+                    }
+                    _package.Add(package);
+                    return RedirectToAction("GetPackage");
+                }
+            }
+            else
+            {
+                ViewBag.Hata = "Başlangıç Tarihi Bitiş Tarihinden Büyük Olamaz.";
+            }
+            return View();
+        }
+
+        public IActionResult GetPackage()
+        {
+            return View(_package.GetAll());
+        }
+
+
+        public IActionResult GetManager()
+        {
+            return View(_person.GetAll());
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateCompany()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompany(CompanyPackage companies)
+        {
+            var company = companies.Company;
+            if (company.Photo == null)
+                company.PhotoName = "\\img\\3586f868_94a2_4fd2_8933_17cd1ff7605e.jpeg";
+            else
+                company.PhotoName = SaveThePicture(company.Photo);
+            _company.Add(company);
+            return RedirectToAction("GetCompanies");
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            return View(_company.GetAll());
+        }
+
+        private string SaveThePicture(IFormFile img)
+        {
+            string filePath = Path.Combine(_env.WebRootPath, "img"); // ~/img
+
+            string uniqueName = $"{Guid.NewGuid().ToString().Replace("-", "_").ToLower()}.{img.ContentType.Split('/')[1]}"; // Benzersiz isim oluşturma. İsimler Guid oluşturulacak. Küçük harf olacak ve - işaretleri yerine _ işareti olacak.
+
+            string newFilePath = Path.Combine(filePath, uniqueName); //~/img/Dosyadı
+
+            using (FileStream fs = new FileStream(newFilePath, FileMode.Create))
+            {
+                img.CopyTo(fs);
+                return newFilePath.Substring(newFilePath.IndexOf("\\img\\")); // burada da dosya yolunun tammaı yerine \img\ kısmını substirng olarak alsın ve return etsin istiyorum. 
+            }
+        }
+
+
     }
 }
