3c2d63c5daa0f3be2586fd47d8b54faef012da11
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-04-10 02:23:27 +0300

    UI katmanına başlandı

---
+++ AtSepete.UI/ApiResponses/BaseResponse/ApiBaseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AtSepete.UI.ApiResponses.BaseResponse
+{
+    public class ApiBaseResponse<T>
+    {
+        public T Data { get; set; }
+        public bool  IsSuccess { get; set; }     
+        public string  Message { get; set; }     
+    }
+}
--- AtSepete.UI/Controllers/HomeController.cs
+++ AtSepete.UI/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using AtSepete.Dtos.Dto.Users;
+﻿using AtSepete.Dtos.Dto.Categories;
+using AtSepete.Dtos.Dto.Users;
 using AtSepete.Entities.BaseData;
 using AtSepete.Entities.Data;
 using AtSepete.Repositories.Abstract;
 using AtSepete.Results;
 using AtSepete.Results.Concrete;
+using AtSepete.UI.ApiResponses;
 using AtSepete.UI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.Text;
 
 namespace AtSepete.UI.Controllers
 {
@@ -27,11 +30,34 @@ namespace AtSepete.UI.Controllers
 
             HttpClient httpClient = new HttpClient();
             HttpResponseMessage response = await httpClient.GetAsync($"https://localhost:7286/AtSepeteApi/user/GetByIdUser/f6c16176-471e-4ef5-1c67-08db38735e6e");
-
             string apiResponse = await response.Content.ReadAsStringAsync();
             UserApiResponse user = JsonConvert.DeserializeObject<UserApiResponse>(apiResponse);           
             return View(user);
 
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCategory(Category category )
+        {
+            CreateCategoryDto dto = new CreateCategoryDto();
+            dto.Description = category.Description;
+            dto.Name = category.Name;
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(dto), Encoding.UTF8, "application/Json");
+                using (var answer = await httpClient.PostAsync("https://localhost:7286/AtSepeteApi/Category/AddCategory", content))
+                {
+                    string apiAnswer = await answer.Content.ReadAsStringAsync();
+                    AddCategoryResponse categoryResponse = JsonConvert.DeserializeObject<AddCategoryResponse>(apiAnswer);
+                    ViewBag.cevap = categoryResponse.Message; 
+                }
+            }
+           
+            return View();
         }
         public IActionResult Privacy()
         {
