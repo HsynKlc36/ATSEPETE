465e24cd83e8d825306b12c791b7a2ed317ac687
Hüseyin Kılıç <huseyin_kilic96@hotmail.com>
2023-06-13 19:18:02 +0300

    alışveriş sepeti toplam tutar hazırlanıyor

---
+++ AtSepete.UI/wwwroot/js/cart_decrease_increase_Handler.js
@@ -0,0 +1,52 @@
+﻿// Decrease button click handler
+function createDecreaseHandler(productId, marketId, quantityInput) {
+    return function () {
+        var quantity = parseInt(quantityInput.value);
+        if (quantity > 1) {
+            quantity--;
+            quantityInput.value = quantity;
+            updateCartItemQuantity(productId, marketId, quantity);
+            
+        } else {
+            removeFromCart(productId, marketId);
+        }
+        renderCartItems();
+    };
+}
+
+
+
+// Increase button click handler
+function createIncreaseHandler(productId, marketId, quantityInput) {
+    return function () {
+        var quantity = parseInt(quantityInput.value);
+        quantity++;
+        quantityInput.value = quantity;
+         updateCartItemQuantity(productId, marketId, quantity);
+        
+        renderCartItems();
+    };
+}
+
+function updateCartItemQuantity(productId, marketId, quantity)
+{
+    var cart = getCartFromLocalStorage();
+
+    // Sepet ürünlerini dolaşarak ilgili ürünü bulun ve adetini güncelleyin
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].productId === productId && cart[i].marketId === marketId) {
+            cart[i].quantity = quantity;
+            break;
+        }
+    }
+
+    // Güncellenmiş sepet verilerini localStorage'a kaydedin
+    saveCartToLocalStorage(cart);
+}
+
+function saveCartToLocalStorage(cart) {
+
+    var cartJson = JSON.stringify(cart);
+    localStorage.setItem('Cart', cartJson);
+
+}
